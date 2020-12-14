366d0e457feadc9e6a8994b06e261a9a34669e14
gaev-art <forsakensr@gmail.com>
2020-12-14 18:43:29 +0300

    removed the background and replaced server url

--- src/api/weatherApi.ts
+++ src/api/weatherApi.ts
@@ -5,18 +5,16 @@ import {LocationWeather, Weather} from '../types/entities'
 export const getWeatherApi = {
   getWeather(cityName: string) {
     return axios.get<LocationWeather>(
-      // `http://localhost:3030/weather?city=${cityName}`)
-      // `https://server-4-weather.herokuapp.com/weather?city=${cityName}`)
-      `https://protected-beyond-90978.herokuapp.com/weather?city=${cityName}`)
+      // `http://localhost:5000/weather?city=${cityName}`)
+      `https://back-4-weather.herokuapp.com/weather?city=${cityName}`)
       .then(response => {
         return response.data
       })
   },
   getHistoryWeather() {
     return axios.get<Weather[]>(
-      // `http://localhost:3030/history`)
-      // `https://server-4-weather.herokuapp.com/history`)
-      `https://protected-beyond-90978.herokuapp.com/history`)
+      // `http://localhost:5000/history`)
+      `https://back-4-weather.herokuapp.com/history`)
       .then(response => {
         return response.data
       })
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,33 @@
-import React from 'react';
-import style from './App.module.css';
-import Header from './c1-header/Header';
-import Weather from './c2-weather/Weather';
-import {Route, Switch} from 'react-router-dom';
-import History from './c3- history/History';
+import React from 'react'
+// import style from './App.module.css'
+import Header from './c1-header/Header'
+import Weather from './c2-weather/Weather'
+import {Route, Switch} from 'react-router-dom'
+import History from './c3- history/History'
 
 function App() {
 
-  const check = function () {
-    const theTime = new Date();
-    const theHour = theTime.getHours();
-
-    if (theHour > 6 && theHour < 12) {
-      return style.morning
-    } else if (theHour > 12 && theHour < 18) {
-      return style.day
-    } else if (theHour > 18 && theHour < 24) {
-      return style.evening
-    } else if (theHour < 6) {
-      return style.night
-    }
-  }
-
-  setInterval(check, 30000);
+  // const check = function () {
+  //   const theTime = new Date()
+  //   const theHour = theTime.getHours()
+  //
+  //   if (theHour > 6 && theHour < 12) {
+  //     return style.morning
+  //   } else if (theHour > 12 && theHour < 18) {
+  //     return style.day
+  //   } else if (theHour > 18 && theHour < 24) {
+  //     return style.evening
+  //   } else if (theHour < 6) {
+  //     return style.night
+  //   }
+  // }
+  //
+  // setInterval(check, 30000)
 
   return (
-    <div className={check()}>
+    <div
+      // className={check()}
+    >
       <Header/>
       <div>
         <Switch>
@@ -36,4 +38,4 @@ function App() {
     </div>)
 }
 
-export default App;
+export default App
