4f72014487f356276e133a00936207fb53309f6c
gaev-art <forsakensr@gmail.com>
2020-12-14 19:08:02 +0300

    refactor

--- src/api/weatherApi.ts
+++ src/api/weatherApi.ts
@@ -2,19 +2,19 @@ import axios from 'axios'
 import {LocationWeather, Weather} from '../types/entities'
 
 
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL
+})
+
 export const getWeatherApi = {
   getWeather(cityName: string) {
-    return axios.get<LocationWeather>(
-      // `http://localhost:5000/weather?city=${cityName}`)
-      `https://back-4-weather.herokuapp.com/weather?city=${cityName}`)
+    return instance.get<LocationWeather>(`/weather?city=${cityName}`)
       .then(response => {
         return response.data
       })
   },
   getHistoryWeather() {
-    return axios.get<Weather[]>(
-      // `http://localhost:5000/history`)
-      `https://back-4-weather.herokuapp.com/history`)
+    return instance.get<Weather[]>(`/history`)
       .then(response => {
         return response.data
       })
