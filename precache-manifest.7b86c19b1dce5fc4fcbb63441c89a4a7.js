04a12c9daf78f13d7a978f6a070a84ffc3524fc2
gaev-art <forsakensr@gmail.com>
2020-12-14 18:47:36 +0300

    Updates

---
+++ precache-manifest.7b86c19b1dce5fc4fcbb63441c89a4a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07420f16c1c2699ca0f4c98699e42806",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "b8e45133e80204bb7bff",
+    "url": "/Weather/static/css/main.fd7ef929.chunk.css"
+  },
+  {
+    "revision": "161ff99010b2e30c46ce",
+    "url": "/Weather/static/js/2.5b5f656e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Weather/static/js/2.5b5f656e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e45133e80204bb7bff",
+    "url": "/Weather/static/js/main.b71f3d41.chunk.js"
+  },
+  {
+    "revision": "33b8acc232781a3d41c0",
+    "url": "/Weather/static/js/runtime-main.22be1482.js"
+  }
+]);
